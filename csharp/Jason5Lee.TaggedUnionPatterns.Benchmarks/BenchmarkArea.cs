9a9b9e5898dbcbbaff73cfbe04b2639ba4ea047d
Jason Dongheng Lee <jason5lee@hotmail.com>
2020-02-12 08:51:49 +0800

    Fix CSharp

--- csharp/Jason5Lee.TaggedUnionPatterns.Benchmarks/BenchmarkArea.cs
+++ csharp/Jason5Lee.TaggedUnionPatterns.Benchmarks/BenchmarkArea.cs
@@ -4,10 +4,9 @@ using BenchmarkDotNet.Jobs;
 
 namespace Jason5Lee.TaggedUnionPatterns.Benchmarks
 {
-    // [SimpleJob(RuntimeMoniker.CoreRt31)]
-    // [SimpleJob(RuntimeMoniker.Mono)]
     [SimpleJob(RuntimeMoniker.NetCoreApp31)]
     [RankColumn]
+    [RPlotExporter]
     public class BenchmarkArea
     {
         [Params(2, 3, 4)]
